71fe9e29c35cbf7d7507e7a3d33276a828d4677d
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2016-11-17 10:11:32 +0100

    Test: sort by 'name' (added PHP test)

--- test/test.php
+++ test/test.php
@@ -43,4 +43,21 @@ class weightSort_test extends PHPUnit_Framework_TestCase {
 
     $this->assertEquals($expected, $actual);
   }
+
+  // sort, key "name" (which are strings)
+  public function testWeightSortName() {
+    global $input;
+    $arr = weightSort($input, 'name');
+
+    $expected = var_export(array(
+      0 => array( 'name' => 'Alice',  'weight' => 62.5, 'height' => 160 ),
+      1 => array( 'name' => 'Bob',    'weight' => 77.0, 'height' => 180 ),
+      2 => array( 'name' => 'Charly', 'weight' => 82.5, 'height' => 180 ),
+      3 => array( 'name' => 'Debby',  'weight' => 62.5, 'height' => 165 ),
+      4 => array( 'name' => 'Earl',   'weight' => 80.0, 'height' => 185 )
+    ), 1);
+    $actual = var_export($arr, 1);
+
+    $this->assertEquals($expected, $actual);
+  }
 }
